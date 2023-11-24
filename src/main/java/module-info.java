76f138a0093ae78488cf9091609486ef22fa9020
vihindi kotalawala <vihindik@gmail.com>
2023-11-24 22:59:30 +0530

    Get club list and corresponding events to dropDown list.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.cw_draft5 {
     requires javafx.fxml;
     requires com.jfoenix;
     requires java.sql;
+    requires mysql.connector.java;
 
 
     opens com.example.cw_draft5 to javafx.fxml;
