9e4a8edc6d76440f2bbd645d0d4d2e295dab559b
Minuri Rajapakse <anjikarajapakse@gmail.com>
2023-11-24 23:04:12 +0530

    Connect the database to the user login and registration, modified the user interfaces.

--- src/main/java/com/example/cw_draft5/LoginController.java
+++ src/main/java/com/example/cw_draft5/LoginController.java
@@ -10,25 +10,34 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.Objects;
 
-public class LoginController {
+
+public class LoginController extends DatabaseConnection {
     @FXML
     public TextField usernameField;
     @FXML
     public PasswordField passwordField;
     @FXML
     public Label validLogin;
+    @FXML
     public Button loginButton;
+    @FXML
+    public Text loginResult;
     @FXML
     private Stage stage;
     @FXML
     private Scene scene;
     @FXML
     private Parent root;
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
 
 
     @FXML
@@ -40,15 +49,56 @@ public class LoginController {
         stage.show();
     }
 
-    public void onClickExistAccount(ActionEvent event) {
-
-    }
-
     public void onClickLogin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Dashboard.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("") && password.equals("")) {
+            loginResult.setText("Please enter the username and password");
+
+        } else {
+            try {
+                pst = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM student WHERE email=? and Password=?");
+                pst.setString(1, username);
+                pst.setString(2, password);
+
+                rs = pst.executeQuery();
+
+                if (rs.next()) {
+                    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Dashboard.fxml")));
+                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                } else {
+                    loginResult.setText("Entered incorrect username or password");
+                    usernameField.setText("");
+                    passwordField.setText("");
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
+//package com.example.cw_draft5;
+//        import java.sql.Connection;
+//        import java.sql.DriverManager;
+//
+//public class DatabaseConnection {
+//    static final String url = "jdbc:mysql://localhost:3306/scms";
+//    static final String user = "root";
+//    static final String password = "";
+//
+//
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            System.out.println("Connecting to the database...");
+//            return DriverManager.getConnection(url, user, password);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error connecting to the database", e);
+//        }
+//    }
+//}
--- src/main/java/com/example/cw_draft5/StudentRegisterController.java
+++ src/main/java/com/example/cw_draft5/StudentRegisterController.java
@@ -2,16 +2,33 @@ package com.example.cw_draft5;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class StudentRegisterController {
     @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
+    public TextField classTeacherNo;
+    public TextField guardianNo;
+    @FXML
     private TextField userLocation;
     @FXML
-    private TextField GuardianName;
+    private TextField guardianName;
 
     @FXML
     private TextField classTeacherName;
@@ -20,7 +37,7 @@ public class StudentRegisterController {
     private TextField contactNo;
 
     @FXML
-    private DatePicker dataOfBirth;
+    private DatePicker dateOfBirth;
 
     @FXML
     private TextField emailAddress;
@@ -39,12 +56,78 @@ public class StudentRegisterController {
 
     @FXML
     private TextArea skills;
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+
 
     @FXML
-    void onClickStudentRegister(ActionEvent event) {
-        AddUserDataIntoDatabase addUserDataIntoDatabase = new AddUserDataIntoDatabase(fullName.getText(),dataOfBirth.getValue(),contactNo.getText(),genderType.getText(),emailAddress.getText(),userLocation.getText(),grade.getText(),classTeacherName.getText(),GuardianName.getText(),skills.getText(),setPassword.getText());
-        addUserDataIntoDatabase.insert();
+    void onClickStudentRegister(ActionEvent event) throws IOException {
+        try {
+            pst = DatabaseConnection.getConnection().prepareStatement("INSERT INTO Student (FullName, DOB, Contact, Gender, Email, Location, Grade, ClassTeacherName, ClassTeacherContact, GuardianName, GuardianContact, Skills, Password) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            pst.setString(1, fullName.getText());
+            pst.setString(2, String.valueOf(dateOfBirth.getValue()));
+            pst.setString(3, contactNo.getText());
+            pst.setString(4, genderType.getText());
+            pst.setString(5, emailAddress.getText());
+            pst.setString(6, userLocation.getText());
+            pst.setString(7, grade.getText());
+            pst.setString(8, classTeacherName.getText());
+            pst.setString(9, classTeacherNo.getText());
+            pst.setString(10, guardianName.getText());
+            pst.setString(11, guardianNo.getText());
+            pst.setString(12, skills.getText());
+            pst.setString(13, setPassword.getText());
 
+            int affectedRows = pst.executeUpdate();
+
+            if (affectedRows>0) {
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Dashboard.fxml")));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Enter all the information");
+                alert.showAndWait();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
+
+
+//if (username.equals("") && password.equals("")) {
+//        loginResult.setText("Please enter the username and password");
+//
+//        } else {
+//        try {
+//        Class.forName("com.mysql.jdbc.Driver");
+//        con = DriverManager.getConnection("jdbc:mysql://localhost/SACMS", "root", "");
+//        pst = con.prepareStatement("SELECT * FROM student WHERE email=? and Password=?");
+//        pst.setString(1, username);
+//        pst.setString(2, password);
+//
+//        rs = pst.executeQuery();
+//
+//        if (rs.next()) {
+//        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Dashboard.fxml")));
+//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+//        } else {
+//        loginResult.setText("Entered incorrect username or password");
+//        usernameField.setText("");
+//        passwordField.setText("");
+//        }
+//
+//        } catch (SQLException | ClassNotFoundException e) {
+//        throw new RuntimeException(e);
+//        }
+//        }
+
