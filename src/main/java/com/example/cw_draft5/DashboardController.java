bf8c9f7d0fe50463e8bdbbc44e7a17578034f331
vihindi kotalawala <vihindik@gmail.com>
2023-11-26 11:37:54 +0530

    Change the DashboardController.java, View the records in the Attendance Tracking table in AttendanceTrackController.java. In EventAttendanceController.java created the attendace tracking system. Created Student.java class.

--- src/main/java/com/example/cw_draft5/DashboardController.java
+++ src/main/java/com/example/cw_draft5/DashboardController.java
@@ -5,17 +5,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import com.jfoenix.controls.JFXButton;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.util.Objects;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
@@ -112,6 +117,17 @@ public class DashboardController implements Initializable {
 
 
     }
+    @FXML
+    public void DashboardClick(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Dashboard.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
 
 
     @FXML
